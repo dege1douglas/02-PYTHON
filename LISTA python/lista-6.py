5c80bdfd78b79ff8746e9800346cc5246aa76fee
Douglas Gomes <117647275+dege1douglas@users.noreply.github.com>
2023-03-02 00:25:35 -0300

    Update lista-6.py

--- LISTA python/lista-6.py
+++ LISTA python/lista-6.py
@@ -1,19 +1,28 @@
-#Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
+#Faça um Programa que peça as quatro notas de 10 alunos
+#, calcule e armazene num vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
 
-par = []
-impar = []
-x=0
-while x!=20:
-    x = x+1
-    numb = int(input('Insira números variados.'))
-    if numb%2 == 0:
-        numb == par.insert(x,numb)
-    else:
-        numb = impar.insert(x,numb)
-x=0
-print('PARES')
-for x in range(len(par)):
-    print(par[x])
-print('IMPARES')
-for x in range(len(impar)):
-    print(impar[x])
+import random
+import time
+notas = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+aluno=[]
+med=[]
+for x in range(0,4): #inserir aluno e gerar contador
+    media=0
+    aluno.insert(x, input(f'NOME ALUNO [{x}]')) #nome aluno
+    for y in range(0,4):  #contador 2 para correr linhas e colunas da matriz
+        notas[x][y] = (int(random.randint(0,10)))
+        media = media + notas[x][y]
+    media=round(media/4 ,1)
+    med.insert(x,media)
+print('-='*35) #auto criar linha divisoria
+start=input('')
+print('ALUNO     ATVD1  ATVD2  ATVD3  ATVD4  MEDIA_F')
+for x in range(0,4): #percorrrer a lista e a matriz(colunas)
+    time.sleep(1)
+    print(f'{aluno[x]:<10}', end='')
+    for y in range(0,4): #percorer matriz(linhas)
+        print(f'{notas[x][y]:<7}', end='')
+    print(f'{med[x]:<7}')
+    print()
+
+    

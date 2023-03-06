3227635290fbb56e41f56504b11d340daebf2196
Douglas Gomes <117647275+dege1douglas@users.noreply.github.com>
2023-03-05 23:20:22 -0300

    Update and rename finalizando-lista-10.py to lista-10.py

--- LISTA python/lista-10.py
+++ LISTA python/lista-10.py
@@ -8,11 +8,7 @@ vtres=[]
 for i in range(10):
     vum.insert(i,int(random.randint(0,9)))
     vdois.insert(i,int(random.randint(10,19)))
-#for i in range(10):
-    #for k in range(19,1,-2):
-        #for h in range(20,0,-2):
-            #vtres.insert(h, vum[i])
-            #vtres.insert(k, vdois[i])
 for i in range(10):
-    print(f'{vum[i]}', end=',')
-    print(f'{vdois[i]}', end=',')
+    vtres.append(vum[i])
+    vtres.append(vdois[i])
+print(vtres)

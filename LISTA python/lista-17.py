2112fc6c5ff3d25a5a78b33769c70e868c988ea6
Douglas Gomes <117647275+dege1douglas@users.noreply.github.com>
2023-03-31 03:34:53 -0300

    Update lista-17.py

--- LISTA python/lista-17.py
+++ LISTA python/lista-17.py
@@ -1,8 +1,7 @@
-#Em uma competição de salto em distância cada atleta tem direito a cinco saltos. 
-#O resultado do atleta será determinado pela média dos cinco valores restantes. 
-#Você deve fazer um programa que receba o nome e as cinco distâncias 
-#alcançadas pelo atleta em seus saltos e depois informe o nome, os saltos e a média dos saltos. 
-#O programa deve ser encerrado quando não for informado o nome do atleta. 
+#Em uma competição de salto em distância cada atleta tem direito a cinco saltos.
+#O resultado do atleta será determinado pela média dos cinco valores restantes.
+#Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas pelo atleta em seus saltos e depois informe o nome,
+#os saltos e a média dos saltos. O programa deve ser encerrado quando não for informado o nome do atleta.
 import random
 salto1=[]
 salto2=[]
@@ -10,23 +9,27 @@ salto3=[]
 salto4=[]
 salto5=[]
 atleta=[]
+med=[]
 media=0
 x=0
-j=1
-for i in range(j):
+while x!=1:
     atleta.append(str(input('Nome do atleta.\n')))
-    if atleta[x]=='':
-        x=1
-        print('Não foi informando um nome válido. Programa finalizado.')
-        break
-    elif atleta[i]!='':
-        j+=1
-        salto1.append(round(random.uniform(3.1,8.9),2))
-        salto2.append(round(random.uniform(3.1,8.9),2))
-        salto3.append(round(random.uniform(3.1,8.9),2))
-        salto4.append(round(random.uniform(3.1,8.9),2))
-        salto5.append(round(random.uniform(3.1,8.9),2))
-        media=salto1[i]+salto2[i]+salto3[i]+salto4[i]+salto5[i]
-    media=round(media/5,2)
-    print(f'{salto1[i]}\n{salto2[i]}\n{salto3[i]}\n{salto4[i]}\n{salto5[i]}\n{media}')
-    
+    for i in range(len(atleta)):
+        if atleta[i]=='':
+            x+=1
+        elif atleta[i]!='':
+            salto1.append(round(random.uniform(3.1,8.9),2))
+            salto2.append(round(random.uniform(3.1,8.9),2))
+            salto3.append(round(random.uniform(3.1,8.9),2))
+            salto4.append(round(random.uniform(3.1,8.9),2))
+            salto5.append(round(random.uniform(3.1,8.9),2))
+            media=salto1[i]+salto2[i]+salto3[i]+salto4[i]+salto5[i]
+            print(f'Atleta:{atleta[i]:^10}\n1º salto:{salto1[i]:>6}\n2º salto:{salto2[i]:>6}\n3º salto:{salto3[i]:>6}\n4º salto:{salto4[i]:>6}\n5º salto:{salto5[i]:>6}')
+            print()
+    med.append(round(media/5,2))
+else:
+    for i in range(len(atleta)):
+        if atleta[i]!='':
+            print(f'Resultado Final:\nAtleta:{atleta[i]:^10}\nSaltos: {salto1[i]} - {salto2[i]} - {salto3[i]} - {salto4[i]} - {salto5[i]}\nMédia dos saltos: {med[i]:>1}')
+            print()
+    print('Programa finalizado.')
